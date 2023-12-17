dff1d005ffe89fb0d1eafa459325ef3eb4820264
Ben Riazy <hamon.riazy@gmail.com>
2023-12-17 16:16:07 +0000

    fix: get the correct env variable

--- src/utils/endpoint.ts
+++ src/utils/endpoint.ts
@@ -1,7 +1,7 @@
 // a file that checks the `PUBLIC_ENV` env variable, if its dev returns a specific base url, if its prod it returns another base url.
 
 export const getBaseUrl = () => {
-  if (process.env.PUBLIC_ENV === "dev") {
+  if (import.meta.env.PUBLIC_ENV === "dev") {
     return "https://y2zegsb3b7.execute-api.us-east-1.amazonaws.com/Prod";
   } else {
     return "https://arxx451fw8.execute-api.us-east-2.amazonaws.com/Prod";
